4cff3498c9cf07c9457aa38340e0c8b40c96f73d
Dolu <doludev@protonmail.com>
2021-06-25 20:22:33 +0200

    Move @vue/compiler-sfc to devDeps + format

--- server/app/Controllers/Http/HomeController.ts
+++ server/app/Controllers/Http/HomeController.ts
@@ -1,9 +1,9 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HomeController {
-    public async index({ inertia }: HttpContextContract) {
-        const brokers = [];
+  public async index({ inertia }: HttpContextContract) {
+    const brokers = []
 
-        return inertia.render('index', { brokers });
-    }
+    return inertia.render('index', { brokers })
+  }
 }
